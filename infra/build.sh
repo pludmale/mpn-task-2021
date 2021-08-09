ff63690658a8f152afbfee909fbf5730522384f7
Katrina K <katrina.kizenbaha@gmail.com>
2021-08-09 11:20:48 +0000

    Setup functional port forwarding and nginx config

--- infra/build.sh
+++ infra/build.sh
@@ -4,4 +4,4 @@ echo "Building the docker image..."
 sudo docker build -t mapon-task-img -f infra/Dockerfile .
 
 echo "Starting the container daemon..."
-sudo docker run -d --name mapon-task-container mapon-task-img
+sudo docker run -d -p 80:80 --name mapon-task-container mapon-task-img
